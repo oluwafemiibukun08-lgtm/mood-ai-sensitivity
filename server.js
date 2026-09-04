539db9cde8061bdd4f07535a12ca6c05c2bd754b
oluwafemiibukun08-lgtm <YOUR_GITHUB_EMAIL>
2026-09-04 15:03:19 +0100

    Serve TikTok verification file

--- server.js
+++ server.js
@@ -8,8 +8,23 @@ const htmlPath = path.join(__dirname, 'public', 'index.html');
 
 console.log('HTML FILE:', htmlPath);
 
-app.get('/privacy', (req, res) => res.sendFile(path.join(__dirname, 'index.html')));
-app.get('/terms', (req, res) => res.sendFile(path.join(__dirname, 'tos.html')));
+app.get('/privacy', (req, res) => {
+    res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+app.get('/terms', (req, res) => {
+    res.sendFile(path.join(__dirname, 'tos.html'));
+});
+
+app.get('/tiktokbhIy9utUg1HP2f4s9C02cIu1tyCgEUGI.txt', (req, res) => {
+    res.type('text/plain').sendFile(
+        path.join(
+            __dirname,
+            'tiktokbhIy9utUg1HP2f4s9C02cIu1tyCgEUGI.txt'
+        )
+    );
+});
+
 app.get('/', (req, res) => {
     console.log('HOME REQUEST');
 
